a22a14a2d529d401919d088cab2d794d34d7d6f1
tariraf <tarisarafika2@gmail.com>
2023-12-16 05:56:42 +0700

    test-nextjs file error, so i made a new project

---
+++ test-nextjs/pages/profile.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout";
+
+export default function Profile() {
+ return (
+  <Layout metaTitle="Profile">
+    <h1 className="py-10">This is Profile!</h1>
+  </Layout>
+ );
+}

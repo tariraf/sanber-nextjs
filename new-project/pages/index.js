dd37d70ede19b00ccadd92619c5445ad1ad72810
tariraf <tarisarafika2@gmail.com>
2023-12-18 09:18:00 +0700

    Tugas day 6

--- new-project/pages/index.js
+++ new-project/pages/index.js
@@ -2,7 +2,7 @@ import Layout from "@/layout";
 
 export default function Home() {
  return (
-  <Layout metaTitle="Home">
+  <Layout metaTitle="Home" metaDescription="This is Home page">
     <h1 className="py-10">This is Home!</h1>
   </Layout>
  );
--- new-project/pages/profile.js
+++ new-project/pages/profile.js
@@ -2,7 +2,7 @@ import Layout from "@/layout";
 
 export default function Profile() {
  return (
-  <Layout metaTitle="Profile">
+  <Layout metaTitle="Profile" metaDescription="This page user information">
     <h1 className="py-10">This is Profile!</h1>
   </Layout>
  );

42d05e63e58caaed422c6f1a9a5ebb3a4ac1375a
tariraf <tarisarafika2@gmail.com>
2023-12-21 13:59:08 +0700

    Tugas day 9

--- new-project/pages/api/hello.js
+++ new-project/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const response = await (await fetch("https://dummyjson.com/users")).json()
+  res.status(200).json({...response})
+  // res.status(200).json({ name: 'John Doe' })
 }
--- new-project/pages/index.js
+++ new-project/pages/index.js
@@ -1,6 +1,17 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(()=> {
+    fetch("/api/hello")
+    .then(res => res.json())
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
  return (
   <Layout metaTitle="Home" metaDescription="This is Home page">
     <div className="h-full">

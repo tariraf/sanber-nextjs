5793725eecbf2573bc3bdd15e75041dff6cc8512
tariraf <tarisarafika2@gmail.com>
2023-12-22 21:21:05 +0700

    Tugas day 10

--- new-project/next.config.js
+++ new-project/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'clipartcraft.com'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- new-project/pages/index.js
+++ new-project/pages/index.js
@@ -1,22 +1,45 @@
 import Layout from "@/layout";
 import { useEffect } from "react";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
 
 export default function Home() {
-  useEffect(()=> {
-    fetch("/api/hello")
-    .then(res => res.json())
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  })
+  const LayoutDynamic = dynamic(() => import("@/layout"))
+  // useEffect(()=> {
+  //   fetch("/api/hello")
+  //   .then(res => res.json())
+  //   .then((res) => {
+  //     console.log(res)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  // })
+
  return (
-  <Layout metaTitle="Home" metaDescription="This is Home page">
+  <LayoutDynamic metaTitle="Home" metaDescription="This is Home page">
     <div className="h-full">
       <h1 className="py-10 text-center text-xl font-extrabold">This is Home!</h1>
+
+      <div className="flex gap-5 justify-center my-5">
+        <div className="text-center">
+          <h3 className="w-60 py-3">Using HTML TAG</h3>
+          <img src="/peach.png" alt="peach image" style={{width: 250}} />
+        </div>
+
+        <div div className="text-center">
+          <h3 className="w-60 py-3">Using Next Image</h3>
+          <Image src="/peach.png" alt="peach image" width={250} height={350} />
+        </div>
+
+        <div div className="text-center">
+          <h3 className="w-60">Using Next Image & URL to get the image</h3>
+          <Image src="https://clipartcraft.com/images/peach-clipart-kawaii.png" alt="peach image" width={250} height={350}/>
+        </div>
+      </div>
+
     </div>
-  </Layout>
+  </LayoutDynamic>
  );
 }
